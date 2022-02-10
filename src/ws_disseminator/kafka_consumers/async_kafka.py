1db4bb0c0347de0cdf9a7caf7a08bec5bcd3d8bc
Ruiwyn <87420965+ruiwynt@users.noreply.github.com>
2022-02-11 01:33:22 +1100

    Kafka consumer has own queue

--- src/ws_disseminator/kafka_consumers/async_kafka.py
+++ src/ws_disseminator/kafka_consumers/async_kafka.py
@@ -1,46 +1,6 @@
 import asyncio
 import threading
-from .kafka_consumer import KafkaConsumer
-
-
-class AsyncConsumer:
-    def __init__(self, kafka_consumer):
-        """Wrap a synchronous kafka consumer in this class to make it asynchronous"""
-        self.waiter = asyncio.Future()
-        self.kafka_consumer = kafka_consumer
-        self.consumer_lock = asyncio.Lock()
-        self.q_size = 0
-    
-    def __repr__(self):
-        return f"AsyncConsumer: topic={self.kafka_consumer.topic}"
-
-    def publish(self, value):
-        waiter, self.waiter = self.waiter, asyncio.Future()
-        waiter.set_result((value, self.waiter))
-    
-    async def run_consumer(self, stop):
-        while not stop.is_set():
-            async with self.consumer_lock:
-                data = await asyncio.to_thread(self.kafka_consumer.consume)
-            if data:
-                self.publish(data)
-                self.q_size += 1
-    
-    async def size(self):
-        return self.q_size
-    
-    async def shutdown(self):
-        async with self.consumer_lock:
-            await asyncio.to_thread(self.kafka_consumer.shutdown)
-
-    async def get_msg(self):
-        waiter = self.waiter
-        while True:
-            value, waiter = await waiter
-            self.q_size -= 1
-            yield value
-
-    __aiter__ = get_msg
+from .kafka_consumer import AsyncKafkaConsumer
 
 
 # {topic: Event}
@@ -48,7 +8,7 @@ events_lock = asyncio.Lock()
 events = {}
 
 async def get_consumer(topic):
-    consumer = AsyncConsumer(KafkaConsumer(topic))
+    consumer = AsyncKafkaConsumer(topic)
     event = threading.Event()
     asyncio.create_task(consumer.run_consumer(event))
     events[topic] = {'consumer': consumer, 'event': event}
@@ -61,9 +21,3 @@ async def shutdown_topic(topic):
         await events[topic]['consumer'].shutdown()
         del events[topic]
     print(f"{topic} consumer stopped")
-
-async def shutdown():
-    async with events_lock:
-        for topic in events.keys():
-            events[topic]['event'].set()
-            await events[topic]['consumer'].shutdown()
--- src/ws_disseminator/kafka_consumers/kafka_consumer.py
+++ src/ws_disseminator/kafka_consumers/kafka_consumer.py
@@ -3,7 +3,7 @@ import sys
 import asyncio
 import threading
 
-class KafkaConsumer():
+class AsyncKafkaConsumer():
     def __init__(self, topic):
         suffix = ""
         if topic not in ("BTCUSDT", "BTCUSD"):
@@ -21,14 +21,34 @@ class KafkaConsumer():
         }
         self.consumer = Consumer(self.conf)
         self.consumer.subscribe([self.topic])
+        self.consumer_lock = asyncio.Lock()
         self.queue = asyncio.Queue()
+        self.closed = False
+
+    def __repr__(self):
+        return f"AsyncKafkaConsumer: topic={self.topic}"
+
+    async def get(self):
+        return await self.queue.get()
     
-    def consume(self):
-        try:
-            return self._consume()
-        except RuntimeError as e:
-            print(e)
-            # print("kafka consumer closed")
+    async def run_consumer(self, stop):
+        while not stop.is_set() and not self.closed:
+            try:
+                async with self.consumer_lock:
+                    msg = await asyncio.to_thread(self._consume)
+                if msg:
+                    await self.queue.put(msg)
+            except RuntimeError as e:
+                print(e)
+                # print("kafka consumer closed")
+    
+    async def shutdown(self):
+        self.closed = True
+        async with self.consumer_lock:
+            await asyncio.to_thread(self.consumer.shutdown)
+    
+    def size(self):
+        return self.queue.qsize()
 
     def _consume(self):
         msg = self.consumer.poll(timeout=10e-9)
@@ -44,17 +64,21 @@ class KafkaConsumer():
         else:
             return msg.value()
     
-    def shutdown(self):
+    async def shutdown(self):
         self.consumer.close()
 
 
-def main():
-    consumer = KafkaConsumer("bybit")
+async def main():
+    consumer = AsyncKafkaConsumer("bybit")
+    print("Starting consumer...")
+    asyncio.create_task(consumer.run_consumer(threading.Event()))
+    print("Consumer started...")
     while True:
-        msg = consumer.consume()
+        print("Awaiting message...")
+        msg = await consumer.get()
         if msg:
             print(msg)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
