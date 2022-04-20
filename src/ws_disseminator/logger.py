b44aefcb84d6232f26f34e4901fcb257b3a051ae
ruiwynt <87420965+ruiwynt@users.noreply.github.com>
2022-04-20 15:16:23 +1000

    Moved server scripts to new repo

--- src/ws_disseminator/logger.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-import sys
-
-def slog(msg):
-    log("server", msg)
-
-def log(s, msg):
-    print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] {s}: {msg}", flush=True)
