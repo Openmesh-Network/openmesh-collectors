918e9227ae8d0ca49f32c6444e888b08fca94988
EzePze <taylorjay098@gmail.com>
2022-10-27 14:36:31 +1100

    updated acks

--- l3_atom/stream_processing/app.py
+++ l3_atom/stream_processing/app.py
@@ -18,7 +18,7 @@ def init():
                         broker=f"aiokafka://{config['KAFKA_BOOTSTRAP_SERVERS']}")
 
     app.conf.consumer_auto_offset_reset = 'latest'
-    app.conf.producer_acks = 0
+    app.conf.producer_acks = 1
 
     codecs.initialise()
     handler.initialise_agents(app)
