31d6661ea3a139ce63699db8daeb9294cccb0ab6
EzePze <taylorjay098@gmail.com>
2022-10-31 19:00:52 +1100

    changed entry point

--- l3_atom/stream_processing/handler.py
+++ l3_atom/stream_processing/handler.py
@@ -8,30 +8,32 @@ handlers = {
     e.exchange.name: e() for e in standardisers
 }
 
-async def process(stream: AsyncIterable) -> AsyncIterable:
-        """
-        Indefinite iterator over the stream of messages coming in over the raw topic. Continuously iterates and processes each incoming message.
 
-        :param stream: The stream of messages to process
-        :type stream: AsyncIterable
-        :return: Iterator over the stream of raw processed messages
-        :rtype: AsyncIterable
-        """
-        async for key, message in stream.items():
-            try:
-                if not key:
-                    continue
-                key = key.decode()
-                exchange = key.split('_', maxsplit=1)[0]
-                standardiser = handlers[exchange]
-                if not standardiser.exchange_started:
-                    standardiser.start_exchange()
-                await standardiser.handle_message(message)
-                yield message
-            except Exception as e:
-                logging.error(f"Error processing message: {e}")
-                logging.error(f"Message: {message}")
+async def process(stream: AsyncIterable) -> AsyncIterable:
+    """
+    Indefinite iterator over the stream of messages coming in over the raw topic. Continuously iterates and processes each incoming message.
+
+    :param stream: The stream of messages to process
+    :type stream: AsyncIterable
+    :return: Iterator over the stream of raw processed messages
+    :rtype: AsyncIterable
+    """
+    async for key, message in stream.items():
+        try:
+            if not key:
                 continue
+            key = key.decode()
+            exchange = key.split('_', maxsplit=1)[0]
+            standardiser = handlers[exchange]
+            if not standardiser.exchange_started:
+                standardiser.start_exchange()
+            await standardiser.handle_message(message)
+            yield message
+        except Exception as e:
+            logging.error(f"Error processing message: {e}")
+            logging.error(f"Message: {message}")
+            continue
+
 
 def initialise_agents(app):
     """Initialises the Faust agent"""
@@ -40,4 +42,3 @@ def initialise_agents(app):
         for topic in standardiser.normalised_topics:
             standardiser.normalised_topics[topic] = app.topic(topic)
     app.agent(RAW_TOPIC)(process)
-
