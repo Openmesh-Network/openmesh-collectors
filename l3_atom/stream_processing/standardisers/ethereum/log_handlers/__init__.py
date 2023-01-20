bf670906f5b49bf979ba0753b4a48694c8c66964
EzePze <taylorjay098@gmail.com>
2023-01-20 17:51:30 +1100

    uniswap-v2 and sushiswap liquidity events

--- l3_atom/stream_processing/standardisers/ethereum/log_handlers/__init__.py
+++ l3_atom/stream_processing/standardisers/ethereum/log_handlers/__init__.py
@@ -5,5 +5,6 @@ from .curve import *
 from .hashflow import *
 
 log_handlers = [UniswapV3SwapHandler, UniswapV2SwapHandler,
+                UniswapV2MintHandler, UniswapV2BurnHandler,
                 UniswapV3MintHandler, UniswapV3BurnHandler,
                 DodoexSellHandler, DodoexBuyHandler, DodoexSwapHandler, CurveSwapHandler, HashflowTradeHandler]
